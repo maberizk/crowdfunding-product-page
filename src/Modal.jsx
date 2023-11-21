304e18260e1037210f4e209f4754818fb3cc7160
maberizk <michelle.aberizk@gmail.com>
2023-11-21 12:11:51 -0800

    modal progress

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -130,7 +130,7 @@ const Modal = ({
           left
         </p>
         {/* </div> */}
-        <p className="description">
+        <p className="description mobileDescription">
           You get an ergonomic stand made of natural bamboo. You've helped us
           launch our promotional campaign, and you'll be added to a special
           Backer member list.
@@ -196,7 +196,7 @@ const Modal = ({
             <p className="cyanText">Pledge $75 or more</p>
           </div>
         </div>
-        <p className="quantityLeftModal">
+        <p className="quantityLeftModal blackEdition">
           <span className="boldedText">
             {itemQuantities && itemQuantities["Black Edition Stand"]}
           </span>
@@ -204,7 +204,7 @@ const Modal = ({
           left
         </p>
         {/* </div> */}
-        <p className="description">
+        <p className="description mobileDescription">
           You get a Black Special Edition computer stand and a personal thank
           you. You'll be added to our Backer member list. Shipping is included.
         </p>
@@ -265,7 +265,7 @@ const Modal = ({
           <span className="boldedText">0</span> left
         </p>
         {/* </div> */}
-        <p className="description">
+        <p className="description mobileDescription">
           You get two Special Edition Mahogany stands, a Backer T-Shirt, and a
           personal thank you. You'll be added to our Backer member list.
           Shipping is included.
